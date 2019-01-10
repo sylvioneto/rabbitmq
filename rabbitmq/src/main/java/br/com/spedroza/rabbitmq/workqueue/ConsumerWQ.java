f2618fc379b80ec4fead6852c14e6a4ce20db939
spedroza <spedroza@SPEDROZA-BR.br.oracle.com>
2019-01-10 11:51:18 -0200

    commit last changes

--- rabbitmq/src/main/java/br/com/spedroza/rabbitmq/workqueue/ConsumerWQ.java
+++ rabbitmq/src/main/java/br/com/spedroza/rabbitmq/workqueue/ConsumerWQ.java
@@ -4,7 +4,6 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
-import com.rabbitmq.client.MessageProperties;
 
 public class ConsumerWQ {
 
